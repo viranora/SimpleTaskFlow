1d13bacfd76be1b02dda2f3d380eab6650b66d99
sema <138892124+viranora@users.noreply.github.com>
2026-01-11 21:47:17 +0300

    Import index.css for styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // <--- BU SATIR YOKSA STİL YOK DEMEKTİR.
+import './index.css' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
